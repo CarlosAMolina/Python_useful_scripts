1b374e55f97e1cb6baa6efdc2bd3406becf21979
CarlosAMolina <carlosantmolina@gmail.com>
2016-03-25 15:37:33 +0100

    pyGetArchivesNameInPath comment
    Improved comment

--- pyGetArchivesNameInPath.py
+++ pyGetArchivesNameInPath.py
@@ -3,7 +3,7 @@
 from os import listdir
 
 def pyGetArchivesNameInPath(archivesPath):  # all archives, example: folders, files, etc
-	# output: list of one or more strings. Or -1 if an error occurs
+	# output: list of strings (no elements -> []). Or -1 if an error occurs
 	try:
 		archivesName = listdir(archivesPath) # if only one elemente it return a list on one string
 	except:
