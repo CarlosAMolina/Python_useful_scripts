cd7e93ff5ccce1812f184c75648758da96bc33ad
CarlosAMolina <carlosantmolina@gmail.com>
2016-03-26 23:16:28 +0100

    Allows filePath to contain the fileName

--- pyDeleteFileMetadata.py
+++ pyDeleteFileMetadata.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python
 
 from pyCommandLine import pyCommandLine
+from os.path import join
 
 # Note: if metadata of the image had been removed before, now the file won't be modified
 
-def pyDeleteFileMetadata(filePath, fileName):
+def pyDeleteFileMetadata(filePath, fileName=None):
 	# variables
-	# input. filePath: str. fileName: str
+	# input. filePath: str. fileName: str or None if filePath contains the path and the fileName
 	# output: int
 	try:
-		fileNameAndPath = filePath+'/'+fileName
+		if fileName == None: # the user puts the filename in filePath
+			fileNameAndPath = filePath
+		else:
+			fileNameAndPath = join(filePath,fileName)
 		command = ['exiftool', '-all=',str(fileNameAndPath)]
 		result = pyCommandLine(command)
 		if 'imagefilesunchanged' in result: # if the metadatas were removed previously, the file won't be modified
